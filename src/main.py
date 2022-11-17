aeeaee140e49f689714724d555e7712cefe3d0bb
Cleyson Costa <cleyson.costa@a4pm.com.br>
2022-11-17 14:37:17 -0300

    port + response

--- src/main.py
+++ src/main.py
@@ -19,9 +19,9 @@ def data(minimum_age: int = 0, maximum_age : int = 115):
 @app.get("/pessoa")
 def pessoa(minimum_age: int = 0, maximum_age : int = 115):
     return {
-        '{"nome": ' + fake.name().replace(".","") + chr(13) +
-        ',"mae": ' + fake.name_female().replace(".","") + chr(13) +
-        ',"pai": ' + fake.name_male().replace(".","") + chr(13) +
-        ',"CPF": ' + fake.cpf().replace(".","").replace("-","") + chr(13) +
-        ',"dtNasci": ' + fake.date_of_birth(None, minimum_age, maximum_age).strftime("%Y-%m-%d") + '}'
+        'nome: ' + fake.name().replace(".","") +
+        ',mae: ' + fake.name_female().replace(".","") +
+        ',pai: ' + fake.name_male().replace(".","") +
+        ',CPF: ' + fake.cpf().replace(".","").replace("-","") +
+        ',dtNasci: ' + fake.date_of_birth(None, minimum_age, maximum_age).strftime("%Y-%m-%d") + '}'
     }
