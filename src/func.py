f2b09bf5fd9b4017a06baf88743ab2f6aac83b49
Cleyson Costa <cleyson.costa@a4pm.com.br>
2022-11-18 09:56:08 -0300

    separar arq func

---
+++ src/func.py
@@ -0,0 +1,49 @@
+from faker import Faker
+
+fake = Faker('pt_BR')
+
+def getName(dblQuotes: bool = True, sex: str = "I"):
+    if (sex == "F"):
+        name = fake.name_female()
+    elif(sex == "M"):
+        name = fake.name_male()
+    else:
+        name = fake.name()
+
+    if dblQuotes:
+        return '"nome": "' + name.replace(".","") + '"'
+    else:
+        return 'nome: ' + name.replace(".","")
+
+def getNameM(dblQuotes: bool = True):
+    if dblQuotes:
+        return '"mae": "' + fake.name_female().replace(".","") + '"'
+    else:
+        return 'mae: ' + fake.name_female().replace(".","")
+
+def getNameF(dblQuotes: bool = True):
+    if dblQuotes:
+        return '"pai": "' + fake.name_male().replace(".","") + '"'
+    else:
+        return 'pai: ' + fake.name_male().replace(".","")
+
+def getCPF(dblQuotes: bool = True, mask: bool = False):
+    if mask:
+        doc = fake.cpf()
+    else:
+        doc = fake.cpf().replace(".","").replace("-","")
+    if dblQuotes:
+        return '"CPF": ' + doc
+    else:
+        return 'CPF: ' + doc
+
+def getDateB(
+    dblQuotes: bool = True,
+    minAge: int = 0,
+    maxAge : int = 115,
+    mask: str = "%Y-%m-%d"
+):
+    if dblQuotes:
+        return '"dtNasci": "' + fake.date_of_birth(None, minAge, maxAge).strftime(mask) + '"'
+    else:
+        return 'dtNasci: ' + fake.date_of_birth(None, minAge, maxAge).strftime(mask)
--- src/main.py
+++ src/main.py
@@ -1,27 +1,72 @@
+import json
 from fastapi import FastAPI
-from faker import Faker
+from fastapi.responses import JSONResponse
+import func
 
 app = FastAPI()
-fake = Faker('pt_BR')
 
 @app.get("/nome")
-def nome():
-    return {'nome: ' + fake.name().replace(".","")}
+def nome(amount: int = 1, sex: str = "I"):
+    listReturn = []
+    for i in range(amount):
+        listReturn.append(func.getName(False, sex))
+    return JSONResponse(listReturn)
+
+@app.get("/pai")
+def pai(amount: int = 1):
+    listReturn = []
+    for i in range(amount):
+        listReturn.append(func.getNameF(False))
+    return JSONResponse(listReturn)
+
+@app.get("/mae")
+def mae(amount: int = 1):
+    listReturn = []
+    for i in range(amount):
+        listReturn.append(func.getNameM(False))
+    return JSONResponse(listReturn)
 
 @app.get("/CPF")
-def cpf():
-    return {'CPF: ' + fake.cpf().replace(".","").replace("-","")}
+def cpf(amount: int = 1, mask: bool = False):
+    listReturn = []
+    for i in range(amount):
+        listReturn.append(func.getCPF(False, mask))
+    return JSONResponse(listReturn)
 
-@app.get("/data")
-def data(minimum_age: int = 0, maximum_age : int = 115):
-    return {'Data: ' + fake.date_of_birth(None, minimum_age, maximum_age).strftime("%Y-%m-%d")}
+@app.get("/dtnasci")
+def dtnasci(
+    minAge: int = 0,
+    maxAge : int = 115,
+    amount: int = 1,
+    mask: str = "%Y-%m-%d"
+):
+    listReturn = []
+    for i in range(amount):
+        listReturn.append(func.getDateB(False, minAge, maxAge, mask))
+    return JSONResponse(listReturn)
 
 @app.get("/pessoa")
-def pessoa(minimum_age: int = 0, maximum_age : int = 115):
-    return {
-        'nome: ' + fake.name().replace(".","") +
-        ',mae: ' + fake.name_female().replace(".","") +
-        ',pai: ' + fake.name_male().replace(".","") +
-        ',CPF: ' + fake.cpf().replace(".","").replace("-","") +
-        ',dtNasci: ' + fake.date_of_birth(None, minimum_age, maximum_age).strftime("%Y-%m-%d") + '}'
-    }
+def pessoa(
+    minAge: int = 0,
+    maxAge: int = 115,
+    amount: int = 1,
+    maskCPF: bool = False,
+    maskDt: str = "%Y-%m-%d"
+):
+    vLoop = True
+    while vLoop:
+        listReturn = []
+        for i in range(amount):
+            listItem = []
+            listItem.append('{' + func.getName(True))
+            listItem.append(func.getNameM(True))
+            listItem.append(func.getNameF(True))
+            listItem.append(func.getCPF(True, maskCPF))
+            listItem.append(func.getDateB(True, minAge, maxAge, maskDt) + '}')
+            try:
+                listReturn.append(json.loads(','.join(listItem)))
+                vLoop = False
+            except:
+                vLoop = True
+            
+    return JSONResponse(listReturn)
